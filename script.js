ccf602b64a14352595361a920f117ed3a23ffddd
KietHub <tuankiet96@hotmail.com>
2022-06-08 23:15:26 +1000

    Add grid colour change function

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const gridContainer = document.querySelector('#grid-container');
 const root = document.querySelector(':root');
+let fill = false;
+
+window.addEventListener('mousedown', (e) => fill = true);
+window.addEventListener('mouseup', (e) => fill = false);
+
+generateGrid(16);   //initial grid generation
 
 function generateGrid(sideLength) {
     for (let i = 0; i < sideLength ** 2; i++) {
         const gridSquare = document.createElement('div');
         gridContainer.appendChild(gridSquare);
         gridSquare.classList.add('grid');
+        gridSquare.addEventListener('mousemove', fillSquare);
     }
-
     root.style.setProperty('--grid-col', sideLength);
 }
 
-generateGrid(16);
+function fillSquare(e) {
+    if(!fill) return;
+    this.classList.add('coloured');
+}
